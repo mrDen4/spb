140b6c2ede47f9956fbd5bc0dc27f41fd9f723ca
Denis Davydov <den.saint.k@gmail.com>
2018-07-28 17:50:13 +0300

    add adaptive styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,21 @@ $('.another__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: $('.prev__btn'),
-    nextArrow: $('.next__btn')
+    nextArrow: $('.next__btn'),
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+  ]
   });
