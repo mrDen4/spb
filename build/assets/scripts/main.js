74a3412cbe191c8b02777a5f0b80d100df3a0a83
Denis Davydov <den.saint.k@gmail.com>
2018-08-03 11:21:04 +0300

    change header and index cards

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -1,3 +1,8 @@
+$('.index__card').on('click', function() {
+  $('.card__menu').css('height', '100px');
+  $('.card__menu').css('padding', '10px');
+})
+
 $('.another__slider').slick({
     infinite: true,
     slidesToShow: 4,
@@ -21,3 +26,4 @@ $('.another__slider').slick({
       }
   ]
   });
+
